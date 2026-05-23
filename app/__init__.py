070a9d82d2d5a36a3fc3345087429c03c88937f3
sandeeppal8192157-oss <sandeeppal8192157@gmail.com>
2026-05-23 20:38:13 +0530

    Added Flask-Login and updated User model

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'my_secret_development_key' 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
     
     db.init_app(app)
     migrate.init_app(app, db)
+    
+    # This turns on the login system
+    login_manager.init_app(app)
+    login_manager.login_view = 'main.login' 
 
-    # Add these two lines:
     from app.routes import main
     app.register_blueprint(main)
     
